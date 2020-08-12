229c037f01f8b24567ee326d070843a4b419bba3
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-12 23:51:29 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears  VALUES (1, "Mr. Chocolate", 1, "m", "color","personality",0);
+INSERT INTO bears  VALUES (2, "Rowdy", 1, "m", "color","personality",0);
+INSERT INTO bears VALUES (3, "Tabitha", 1, "m", "color","personality",1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 1, "m", "color","personality",0);
+INSERT INTO bears VALUES (5, "Melissa", 1, "m", "color","personality",0);
+INSERT INTO bears VALUES (6, "Grinch", 1, "m", "color","personality",1);
+INSERT INTO bears VALUES (7, "Wendy", 1, "m", "color","personality",0);
+INSERT INTO bears VALUES (8,null, 1, "m", "color","personality",0);

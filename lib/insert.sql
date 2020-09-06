97a08e76d5405c424c132a10a330869d222b5342
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-06 23:29:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 INSERT INTO bears  VALUES (1, "Mr. Chocolate", 1, "m", "color","personality",0);
 INSERT INTO bears  VALUES (2, "Rowdy", 1, "m", "color","personality",0);
 INSERT INTO bears VALUES (3, "Tabitha", 1, "m", "color","personality",1);
@@ -6,3 +7,14 @@ INSERT INTO bears VALUES (5, "Melissa", 1, "m", "color","personality",0);
 INSERT INTO bears VALUES (6, "Grinch", 1, "m", "color","personality",1);
 INSERT INTO bears VALUES (7, "Wendy", 1, "m", "color","personality",0);
 INSERT INTO bears VALUES (8,null, 1, "m", "color","personality",0);
+=======
+INSERT INTO bears  VALUES (8, "Mr. Chocolate", 1, "m", "color","personality",0);
+INSERT INTO bears  VALUES (8, "Rowdy", 1, "m", "color","personality",0);
+INSERT INTO bears  VALUES (8, "Tabitha", 1, "m", "color","personality",0);
+INSERT INTO bears  VALUES (8, "Sergeant Brown", 1, "m", "color","personality",0);
+INSERT INTO bears  VALUES (8, "Melissa", 1, "m", "color","personality",0);
+INSERT INTO bears  VALUES (8, "Grinch", 1, "m", "color","personality",0);
+INSERT INTO bears  VALUES (8, "Wendy", 1, "m", "color","personality",0);
+INSERT INTO bears  VALUES (8, NULL, 1, "m", "color","personality",0
+);
+>>>>>>> 874d902fb20c6db4d6fda99a879ea41f3df37b41
